ef51f4ecd7d63a7159420b344675701e2169f2e1
jcl1ent <jclient11@gmail.com>
2026-02-19 19:01:34 +0800

    Created env for security details on DB connection

--- dbcon.php
+++ dbcon.php
@@ -1,5 +1,9 @@
 <?php
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$name = getenv("DB_NAME");
 
-$con = mysqli_connect("localhost","root","root","traders");
+$con = mysqli_connect($host,$user,$pass,$name);
 
 ?>
